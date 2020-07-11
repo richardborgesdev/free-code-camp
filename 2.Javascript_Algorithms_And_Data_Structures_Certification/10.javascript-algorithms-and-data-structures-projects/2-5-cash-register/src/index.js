b24cf943cd55a94855763bf5be99a27d16e7ab0d
Richard Borges <richard.borges@contaazul.com>
2020-07-11 10:09:26 -0300

    certification: cash register, add register cash out status and logs to compare to test result

--- 2.Javascript_Algorithms_And_Data_Structures_Certification/10.javascript-algorithms-and-data-structures-projects/2-5-cash-register/src/index.js
+++ 2.Javascript_Algorithms_And_Data_Structures_Certification/10.javascript-algorithms-and-data-structures-projects/2-5-cash-register/src/index.js
@@ -31,6 +31,10 @@ const registerOut = (cid, changeRegister) => {
       change.push(cid[index]);
     } else if (changeRegister[index][1] > 0) {
       change.push(changeRegister[index]);
+      status = "OPEN";
+    } else if (cid[index][1] < 0) {
+      change.push(cid[index]);
+      status = "INSUFFICIENT_FUNDS";
     }
   }
 
@@ -65,6 +69,7 @@ const checkCashRegister = (price, cash, cid) => {
 };
 
 console.log(
+  "1,2",
   checkCashRegister(19.5, 20, [
     ["PENNY", 1.01],
     ["NICKEL", 2.05],
@@ -75,5 +80,94 @@ console.log(
     ["TEN", 20],
     ["TWENTY", 60],
     ["ONE HUNDRED", 100]
-  ])
+  ]),
+  { status: "OPEN", change: [["QUARTER", 0.5]] }
+);
+
+console.log(
+  3,
+  checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+  ]),
+  {
+    status: "OPEN",
+    change: [
+      ["TWENTY", 60],
+      ["TEN", 20],
+      ["FIVE", 15],
+      ["ONE", 1],
+      ["QUARTER", 0.5],
+      ["DIME", 0.2],
+      ["PENNY", 0.04]
+    ]
+  }
+);
+
+console.log(
+  4,
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]),
+  { status: "INSUFFICIENT_FUNDS", change: [] }
+);
+
+console.log(
+  5,
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 0.01],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 1],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]),
+  { status: "INSUFFICIENT_FUNDS", change: [] }
+);
+
+console.log(
+  6,
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]),
+  {
+    status: "CLOSED",
+    change: [
+      ["PENNY", 0.5],
+      ["NICKEL", 0],
+      ["DIME", 0],
+      ["QUARTER", 0],
+      ["ONE", 0],
+      ["FIVE", 0],
+      ["TEN", 0],
+      ["TWENTY", 0],
+      ["ONE HUNDRED", 0]
+    ]
+  }
 );
