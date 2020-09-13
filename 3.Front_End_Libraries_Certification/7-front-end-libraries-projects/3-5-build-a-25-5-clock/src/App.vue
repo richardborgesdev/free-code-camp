7a382fad9b1c099ef88c8a4ffc8d7b26634ea727
Richard Borges <richard.borges@contaazul.com>
2020-09-13 11:32:09 -0300

    wip: clock 25 + 5 get start

---
+++ 3.Front_End_Libraries_Certification/7-front-end-libraries-projects/3-5-build-a-25-5-clock/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Clock255 msg="Hello Vue in CodeSandbox!"/>
+  </div>
+</template>
+
+<script>
+import Clock255 from "./components/Clock255";
+
+export default {
+  name: "App",
+  components: {
+    Clock255
+  },
+  mounted() {
+    let externalScript = document.createElement("script");
+    externalScript.setAttribute(
+      "src",
+      "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"
+    );
+    document.head.appendChild(externalScript);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
