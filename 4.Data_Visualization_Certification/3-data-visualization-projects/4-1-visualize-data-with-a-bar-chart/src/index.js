240e9cfdeed96588344bd7b43ea585905945aab6
Richard Borges <richard.borges@contaazul.com>
2020-10-28 13:41:47 -0300

    wip: add data visualization axis

--- 4.Data_Visualization_Certification/3-data-visualization-projects/4-1-visualize-data-with-a-bar-chart/src/index.js
+++ 4.Data_Visualization_Certification/3-data-visualization-projects/4-1-visualize-data-with-a-bar-chart/src/index.js
@@ -1,32 +1,84 @@
 import * as d3 from "d3";
 
-const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+const dataset = [
+  [34, 78],
+  [109, 280],
+  [310, 120],
+  [79, 411],
+  [420, 220],
+  [233, 145],
+  [333, 96],
+  [222, 333],
+  [78, 320],
+  [21, 123]
+];
 
 const w = 500;
-const h = 100;
+const h = 500;
+const padding = 60;
+
+const xScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset, (d) => d[0])])
+  .range([padding, w - padding]);
+
+const yScale = d3
+  .scaleLinear()
+  .domain([0, d3.max(dataset, (d) => d[1])])
+  .range([h - padding, padding]);
 
 const svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
 svg
-  .selectAll("rect")
+  .selectAll("circle")
   .data(dataset)
   .enter()
-  .append("rect")
-  .attr("x", 0)
-  .attr("y", 0)
-  .attr("width", 25)
-  .attr("height", 100);
+  .append("circle")
+  .attr("cx", (d) => xScale(d[0]))
+  .attr("cy", (d) => yScale(d[1]))
+  .attr("r", (d) => 5);
+
+svg
+  .selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text((d) => d[0] + "," + d[1])
+  .attr("x", (d) => xScale(d[0] + 10))
+  .attr("y", (d) => yScale(d[1]));
+
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yScale);
 
 /*
   User Story #2: My chart should have a g element x-axis with a corresponding
   id="x-axis".
+*/
+svg
+  .append("g")
+  .attr("id", "x-axis")
+  /*
+    User Story #4: Both axes should contain multiple tick labels, each with the
+    corresponding class="tick".
+  */
+  .attr("transform", "translate(0," + (h - padding) + ")")
+  .call(xAxis);
 
+/*
   User Story #3: My chart should have a g element y-axis with a corresponding
   id="y-axis".
+*/
+svg
+  .append("g")
+  .attr("id", "y-axis")
+  /*
+      User Story #4: Both axes should contain multiple tick labels, each with the
+      corresponding class="tick".
+    */
+  .attr("transform", "translate(" + padding + ", 0)")
+  .call(yAxis);
 
-  User Story #4: Both axes should contain multiple tick labels, each with the
-  corresponding class="tick".
-
+/*
   User Story #5: My chart should have a rect element for each data point with
   a corresponding class="bar" displaying the data.
 
