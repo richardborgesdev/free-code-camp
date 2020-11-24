20c8b7792f8fd7d4728b3f98db5177ee33d1e98d
Richard Borges <richard.borges@contaazul.com>
2020-11-24 13:31:52 -0300

    wip: fill heat maps cells with colors

--- 4.Data_Visualization_Certification/3-data-visualization-projects/4-3-visualize-data-with-a-heat-map/src/index.js
+++ 4.Data_Visualization_Certification/3-data-visualization-projects/4-3-visualize-data-with-a-heat-map/src/index.js
@@ -135,6 +135,151 @@ const buildVisualization = (dataset) => {
     .style("text-anchor", "middle")
     .attr("transform", "translate(" + width / 2 + "," + 3 * fontSize + ")");
 
+  var colorbrewer = {
+    RdYlBu: {
+      3: ["#fc8d59", "#ffffbf", "#91bfdb"],
+      4: ["#d7191c", "#fdae61", "#abd9e9", "#2c7bb6"],
+      5: ["#d7191c", "#fdae61", "#ffffbf", "#abd9e9", "#2c7bb6"],
+      6: ["#d73027", "#fc8d59", "#fee090", "#e0f3f8", "#91bfdb", "#4575b4"],
+      7: [
+        "#d73027",
+        "#fc8d59",
+        "#fee090",
+        "#ffffbf",
+        "#e0f3f8",
+        "#91bfdb",
+        "#4575b4"
+      ],
+      8: [
+        "#d73027",
+        "#f46d43",
+        "#fdae61",
+        "#fee090",
+        "#e0f3f8",
+        "#abd9e9",
+        "#74add1",
+        "#4575b4"
+      ],
+      9: [
+        "#d73027",
+        "#f46d43",
+        "#fdae61",
+        "#fee090",
+        "#ffffbf",
+        "#e0f3f8",
+        "#abd9e9",
+        "#74add1",
+        "#4575b4"
+      ],
+      10: [
+        "#a50026",
+        "#d73027",
+        "#f46d43",
+        "#fdae61",
+        "#fee090",
+        "#e0f3f8",
+        "#abd9e9",
+        "#74add1",
+        "#4575b4",
+        "#313695"
+      ],
+      11: [
+        "#a50026",
+        "#d73027",
+        "#f46d43",
+        "#fdae61",
+        "#fee090",
+        "#ffffbf",
+        "#e0f3f8",
+        "#abd9e9",
+        "#74add1",
+        "#4575b4",
+        "#313695"
+      ]
+    },
+    RdBu: {
+      3: ["#ef8a62", "#f7f7f7", "#67a9cf"],
+      4: ["#ca0020", "#f4a582", "#92c5de", "#0571b0"],
+      5: ["#ca0020", "#f4a582", "#f7f7f7", "#92c5de", "#0571b0"],
+      6: ["#b2182b", "#ef8a62", "#fddbc7", "#d1e5f0", "#67a9cf", "#2166ac"],
+      7: [
+        "#b2182b",
+        "#ef8a62",
+        "#fddbc7",
+        "#f7f7f7",
+        "#d1e5f0",
+        "#67a9cf",
+        "#2166ac"
+      ],
+      8: [
+        "#b2182b",
+        "#d6604d",
+        "#f4a582",
+        "#fddbc7",
+        "#d1e5f0",
+        "#92c5de",
+        "#4393c3",
+        "#2166ac"
+      ],
+      9: [
+        "#b2182b",
+        "#d6604d",
+        "#f4a582",
+        "#fddbc7",
+        "#f7f7f7",
+        "#d1e5f0",
+        "#92c5de",
+        "#4393c3",
+        "#2166ac"
+      ],
+      10: [
+        "#67001f",
+        "#b2182b",
+        "#d6604d",
+        "#f4a582",
+        "#fddbc7",
+        "#d1e5f0",
+        "#92c5de",
+        "#4393c3",
+        "#2166ac",
+        "#053061"
+      ],
+      11: [
+        "#67001f",
+        "#b2182b",
+        "#d6604d",
+        "#f4a582",
+        "#fddbc7",
+        "#f7f7f7",
+        "#d1e5f0",
+        "#92c5de",
+        "#4393c3",
+        "#2166ac",
+        "#053061"
+      ]
+    }
+  };
+  var legendColors = colorbrewer.RdYlBu[11].reverse();
+  var variance = dataset.monthlyVariance.map(function (val) {
+    return val.variance;
+  });
+  var minTemp = dataset.baseTemperature + Math.min.apply(null, variance);
+  var maxTemp = dataset.baseTemperature + Math.max.apply(null, variance);
+  var legendThreshold = d3
+    .scaleThreshold()
+    .domain(
+      (function (min, max, count) {
+        var array = [];
+        var step = (max - min) / count;
+        var base = min;
+        for (var i = 1; i < count; i++) {
+          array.push(base + i * step);
+        }
+        return array;
+      })(minTemp, maxTemp, legendColors.length)
+    )
+    .range(legendColors);
+
   svg
     .append("g")
     .classed("map", true)
@@ -170,6 +315,9 @@ const buildVisualization = (dataset) => {
       height: function (d) {
         return yScale.rangeBand(d.month);
       }
+    })
+    .attr("fill", function (d) {
+      return legendThreshold(dataset.baseTemperature + d.variance);
     });
 };
 
