42fa131600497b3c8c04cc4560c052b4bd39dcdd
richard borges <richard.borges.cc@gmail.com>
2021-01-12 10:37:57 -0300

    wip: remove unused code

--- 5.Apis_And_Microservices_Certification/3-apis-and-microservices-projects/5-2-request-header-parser-microservice/src/index.js
+++ 5.Apis_And_Microservices_Certification/3-apis-and-microservices-projects/5-2-request-header-parser-microservice/src/index.js
@@ -12,21 +12,8 @@
 
 var http = require("http");
 
-const timestampToUTC = (d) => {
-  const intDate = parseInt(d, 10);
-  return new Date(intDate).toUTCString();
-};
-
-/*
-  Your project can handle dates that can be successfully parsed
-  by new Date(date_string)
-*/
-const isValidDate = (d) => new Date(d).toString() !== "Invalid Date";
-
-const toTimestampDate = (d) => new Date(d).getTime();
-
 const routes = {
-  "/timestamp:get": async (request, response) => {
+  "/whoami:get": async (request, response) => {
     const { url, method } = request;
     console.log(url, method);
 
@@ -34,51 +21,7 @@ const routes = {
     const [, first, route, data] = url.split("/");
     console.log(first, route, data);
 
-    /*
-      If the input date string is invalid, the api returns an object
-      having the structure { error : "Invalid Date" }
-    */
     let resolved = { error: "Invalid Date" };
-
-    if (!data) {
-      /*
-        An empty date parameter should return the current time in
-        a JSON object with a unix key
-
-        An empty date parameter should return the current time in
-        a JSON object with a utc key
-      */
-      const todayDate = Date.now();
-      resolved = {
-        unix: todayDate,
-        utc: timestampToUTC(todayDate)
-      };
-    } else if (data.match(/^\d*$/)) {
-      /*
-        A request to /api/timestamp/1451001600000 should
-        return { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
-      */
-      const intDate = parseInt(data, 10);
-
-      resolved = {
-        unix: intDate,
-        utc: timestampToUTC(data)
-      };
-    } else if (isValidDate(data)) {
-      /*
-        A request to /api/timestamp/:date? with a valid date should return
-        a JSON object with a unix key that is a Unix timestamp of the
-        input date in milliseconds
-
-        A request to /api/timestamp/:date? with a valid date should return
-        a JSON object with a utc key that is a string of the input date in
-        the format: Thu, 01 Jan 1970 00:00:00 GMT
-      */
-      resolved = {
-        unix: toTimestampDate(data),
-        utc: new Date(data).toUTCString()
-      };
-    }
 
     console.log(JSON.stringify(resolved));
     response.write(JSON.stringify(resolved));
