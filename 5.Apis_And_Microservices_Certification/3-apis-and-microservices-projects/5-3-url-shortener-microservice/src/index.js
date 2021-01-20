1e539c10340900ff61686c2ff9af9fcb86b71a14
Richard Borges <richard.borges@contaazul.com>
2021-01-20 13:46:51 -0300

    wip: add tests descriptions

--- 5.Apis_And_Microservices_Certification/3-apis-and-microservices-projects/5-3-url-shortener-microservice/src/index.js
+++ 5.Apis_And_Microservices_Certification/3-apis-and-microservices-projects/5-3-url-shortener-microservice/src/index.js
@@ -1,4 +1,16 @@
 /*
+  You can POST a URL to /api/shorturl/new and get a JSON response
+  with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
+
+  When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+
+  If you pass an invalid URL that doesn't follow the valid http://www.example.com format,
+  the JSON response will contain { error: 'invalid url' }
+
+  HINT: Do not forget to use a body parsing middleware to handle the POST requests.
+  Also, you can use the function dns.lookup(host, cb) from the dns core module
+  to verify a submitted URL.
+
   test: https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/url-shortener-microservice
 */
 
